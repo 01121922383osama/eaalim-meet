518474cbb5e4f0a04fad66fd74c008c7453cfdcc
Osama Nabil <121755701+01121922383osama@users.noreply.github.com>
2024-10-20 12:42:19 +0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1553211988"
+    serviceWorkerVersion: "1900141162"
   }
 });
